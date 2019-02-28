bb8c81745acde35f2c865c1bad498deded8043fd
Thomas Goyne <tg@realm.io>
2019-02-28 13:42:26 -0800

    stop

--- tests/js/notifier-tests.ts
+++ tests/js/notifier-tests.ts
@@ -765,8 +765,8 @@ describe('Multi-process Notifier', () => {
     });
 
     afterEach(async function() {
-        await worker.stop();
         await Realm.Sync.removeAllListeners();
+        await worker.stop();
         if (rosController) {
             await rosController.shutdown();
         }
