7a75e06a26f81fb3fdf61d21c4e4f317556324dd
Talha Zekeriya Durmuş <drtzack@gmail.com>
2019-07-11 23:05:05 +0300

    Create react-native.config.js

---
+++ react-native.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  project: {
+    "ios": {
+      "project": "react-native/ios/RealmReact.xcodeproj",
+      "sharedLibraries": [
+        "libc++",
+        "libz"
+      ]
+    },
+    android: {},
+  },
+  assets: [],
+};
